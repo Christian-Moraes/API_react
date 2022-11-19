3bedc5b768f5e613e7d5c03c15648ba0b5b8c9a3
Christian Moraes <christianmath2001@gmail.com>
2022-11-19 00:16:25 -0300

    ajuste_DBO

--- conexao.php
+++ conexao.php
@@ -1,11 +1,13 @@
 <?php
-$host = 'localhost';
+$host = 'containers-us-west-128.railway.app';
 $user = 'root';
-$pass = 'root';
+$pass = 'dXUdp1YYjwJR2vokuVDS';
 $dbname = 'api';
+$port = '7863';
+$dsn = "mysql:host=".$host.";dbname=".$dbname.";charset=utf8mb4;port=".$port;
 
 try {
-  $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+  $conn = new PDO($dsn . $dbname, $user, $pass);
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
       echo 'ERROR: ' . $e->getMessage();
